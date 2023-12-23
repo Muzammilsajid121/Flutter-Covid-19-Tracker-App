c80d9f2f7b461ee6cca9eabe7dc2014fe034259c
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2023-12-23 22:06:30 +0500

    Device responsive

--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -40,83 +40,92 @@ class _WorldStatesState extends State<WorldStates> with TickerProviderStateMixin
 final height = MediaQuery.sizeOf(context).height *1;
 final width = MediaQuery.sizeOf(context).width *1;
     return  Scaffold(
-      body: Column(
-        children: [
-           SizedBox(height: MediaQuery.of(context).size.height*0.04,),
-           //
-           FutureBuilder(
-            future: statesServices.fetchWorldStatesApi(), 
-            builder: (context, AsyncSnapshot<WorldStatesModel> snapshot){
-
-              if(!snapshot.hasData){
-                return  Center(child:  SpinKitFadingCube(
-                color: Color.fromARGB(255, 218, 89, 132),
-                size: height*0.06,));
-
-              }else{
-                return Column(
-                  children: [
-                    //
-                     //
-           PieChart(
-          chartType: ChartType.ring,
-          chartValuesOptions: const ChartValuesOptions(
-            showChartValuesInPercentage: true
-          ),
-          chartRadius:width*0.3,
-          legendOptions: const LegendOptions(
-            legendPosition: LegendPosition.left ),
-            colorList: colorList,
-            dataMap:  {
-              
-               "Total" : double.parse(snapshot.data!.cases.toString()),
-               "Recovered" : double.parse(snapshot.data!.recovered.toString()),
-              "Deaths": double.parse(snapshot.data!.deaths.toString()),
-            },
-            animationDuration: const  Duration(milliseconds: 1200),
-            ),
-            //
-
-            Padding(
-              padding:  EdgeInsets.symmetric(horizontal: width*0.04, vertical: height*0.02),
-              child: Card(
-                color: const Color.fromARGB(255, 66, 66, 66),
-                child: Column(
-                  children: [
-                         ReusableRow(title: 'Total', value: snapshot.data!.cases.toString()),
-                         ReusableRow(title: 'Recovered', value: snapshot.data!.recovered.toString()),
-                         ReusableRow(title: 'Deaths', value: snapshot.data!.deaths.toString()),
-                         ReusableRow(title: 'Active Cases', value: snapshot.data!.active.toString()),
-                         ReusableRow(title: 'Today Deaths', value: snapshot.data!.todayDeaths.toString()),
-                         ReusableRow(title: 'Today Recovered', value: snapshot.data!.todayRecovered.toString()),
-                  ],
+      appBar: AppBar(
+        title: Text('Covid-19 World Stats',style: Theme.of(context).textTheme.bodyLarge,),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+             SizedBox(height: MediaQuery.of(context).size.height*0.09,),
+             //
+             FutureBuilder(
+              future: statesServices.fetchWorldStatesApi(), 
+              builder: (context, AsyncSnapshot<WorldStatesModel> snapshot){
+             
+                if(!snapshot.hasData){
+                  return  SpinKitDualRing(
+                  color: const Color.fromARGB(255, 212, 209, 3),
+                  size: height*0.04,);
+             
+                }else{
+                  return Column(
+                    children: [
+                      //
+                       //
+             PieChart(
+                       chartType: ChartType.ring,
+                       chartValuesOptions: const ChartValuesOptions(
+              showChartValuesInPercentage: true
+                       ),
+                       chartRadius:width*0.3,
+                       legendOptions: const LegendOptions(
+              legendPosition: LegendPosition.left ),
+              colorList: colorList,
+              dataMap:  {
+                
+                 "Total" : double.parse(snapshot.data!.cases.toString()),
+                 "Recovered" : double.parse(snapshot.data!.recovered.toString()),
+                "Deaths": double.parse(snapshot.data!.deaths.toString()),
+              },
+              animationDuration: const  Duration(milliseconds: 1200),
+              ),
+              //
+             
+              Padding(
+                padding:  EdgeInsets.symmetric(horizontal: width*0.04, vertical: height*0.02),
+                child: Card(
+                  color: const Color.fromARGB(255, 66, 66, 66),
+                  child: Column(
+                    children: [
+                           ReusableRow(title: 'Total', value: snapshot.data!.cases.toString()),
+                           ReusableRow(title: 'Recovered', value: snapshot.data!.recovered.toString()),
+                           ReusableRow(title: 'Deaths', value: snapshot.data!.deaths.toString()),
+                           ReusableRow(title: 'Active Cases', value: snapshot.data!.active.toString()),
+                           ReusableRow(title: 'Today Deaths', value: snapshot.data!.todayDeaths.toString()),
+                           ReusableRow(title: 'Today Recovered', value: snapshot.data!.todayRecovered.toString()),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            //BUTTON
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SizedBox(
-                height: height*0.07, width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white
-                  ),
-                  onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const  CountriesList2()));
-                  }, 
-                  child:const  Text("Track countries")),
-              ),
-            )
-                  ],
-                );
-
-              }
-
-            }),
-
-        ],
+              //BUTTON
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  height: height*0.07, width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white
+                    ),
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const  CountriesList2()));
+                    }, 
+                    child:const  Text("Track countries")),
+                ),
+              )
+             
+                    ],
+                  );
+             
+                }
+             
+              }),
+        
+          ],
+        ),
       ),
     );
   }
